4c4c94b337675f4ff67660f9280bd1f4035929ea
Harshal Shah <harshalshah@Harshal-Shahs-MacBook-Pro.local>
2015-12-20 17:13:26 +1100

    add new card

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var fs = require('fs');
 var express = require('express');
 var _ = require('lodash');
+var bodyParser = require('body-parser');
 
 //express app initialization
 var app = express();
+app.use(bodyParser.json());
 
 //routes
 app.get('/', function (req, res) {
@@ -40,6 +42,28 @@ app.get('/cards/:categoryId', function (req, res) {
   });
 
 });
+
+//Add Card
+app.post('/addCard', function(req, res) {
+  var cards;
+  fs.readFile('card.json', function(error, data) {
+    if(error) {
+      throw error;
+    };
+
+    cards = JSON.parse(data);
+    cards.push(req.body);
+
+    // null - represents the replacer function. (in this case we don't want to alter the process)
+    // 2 - represents the spaces to indent.
+    fs.writeFileSync('card.json', JSON.stringify(cards, null,  2));
+
+    res.status(200);
+    res.end();
+
+  });
+});
+
 
 
   //Server start
